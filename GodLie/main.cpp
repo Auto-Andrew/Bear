688ec41fd194086280fb7cd6802fc406ec4380f9
Auto-Andrew <57337251+Auto-Andrew@users.noreply.github.com>
2020-09-07 17:53:46 +0800

    Add files via upload

---
+++ GodLie/main.cpp
@@ -0,0 +1,11 @@
+#include "widget.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Widget w;
+    w.show();
+    w.setFixedSize(1294, 648);
+    return a.exec();
+}
